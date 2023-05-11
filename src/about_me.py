a92ef73a647b8bc6354dcd458e70c05c8d7f3af4
CAMM961001 <miguel.castama@gmail.com>
2023-05-11 17:00:35 -0500

    Replace charts with previously generated images

--- src/about_me.py
+++ src/about_me.py
@@ -1,8 +1,8 @@
-import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
+from PIL import Image
 from .settings import Settings
 
 
@@ -50,84 +50,11 @@ class AboutMe:
         # Section description
         st.write(self.description[1], unsafe_allow_html=True)
 
-        # Open timeline asset file
-        with open(settings.config['path']['timeline'], 'r') as file:
-            data = json.load(file)
-        file.close()
+        # Load previously generated image
+        fig_ = Image.open(settings.config['images']['timeline'])
 
-        # Data preprocess for visualization
-        data = (
-            pd.DataFrame(data)
-            .assign(
-                inicio = lambda df_: pd.to_datetime(df_.inicio, format='%d/%m/%Y')
-                ,fin = lambda df_: pd.to_datetime(df_.fin, format='%d/%m/%Y')
-                ,clase = lambda df_: df_.clase.astype(int)
-                ,delta = lambda df_: df_.delta.astype(int)))
-        
-        # Figure specs
-        fig, ax = plt.subplots(figsize=(16,6))
-        ymin, ymax = data.clase.min() - 0.5, data.clase.max() + 0.5
-        covid_x = pd.to_datetime('20/03/2020', format='%d/%m/%Y')
-        
-        # Covid pandemics reference line
-        ax.axvline(
-            x=covid_x
-            ,color='red',
-            alpha=0.4)
-        
-        # Covid pandemics annotation
-        ax.text(
-            x=covid_x
-            ,y=ymin + 0.3
-            ,s=' Coronavirus isolation\n is declared in Mexico',
-            alpha=0.4)
-
-        # Today reference line
-        ax.axvline(
-            x=self.today
-            ,color='red',
-            alpha=0.4)
-
-        # Today annotation
-        ax.text(
-            x=self.today
-            ,y=1.3
-            ,s=' The best is\n yet to come...',
-            alpha=0.4)
-
-        for idx in range(data.shape[0]):
-            # Gráficas
-            ax.plot(
-                [data.inicio[idx], data.fin[idx]]
-                ,[data.clase[idx], data.clase[idx]]
-                ,linewidth=6
-                ,color=data.color[idx])
-            
-            ax.scatter(
-                x=[data.inicio[idx], data.fin[idx]]
-                ,y=[data.clase[idx], data.clase[idx]]
-                ,color=data.color[idx]
-                ,s=100)
-            
-            ax.text(
-                x=data.inicio[idx]
-                ,y=data.clase[idx] + 0.35*data.delta[idx]
-                ,s=data.nombre[idx]
-                ,va='center')
-
-        # Figure annotations and styling
-        ax.set_ylim(bottom=ymin ,top=ymax)
-        ax.set_yticks(
-            ticks=data.clase.unique()
-            ,labels=data.empresa.unique())
-        
-        ax.grid(axis='y', alpha=0.25)
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        ax.spines['left'].set_visible(False)
-
-        # Render streamlit object
-        st.pyplot(fig=fig, clear_figure=True)
+        # Render image
+        st.image(fig_)
 
 
     def skills(self):
@@ -136,120 +63,39 @@ class AboutMe:
 
         # Section description
         st.write(self.description[2], unsafe_allow_html=True)
-
-        skill_level = {
-                    1:'Recruit'
-                    ,2:'Regular'
-                    ,3:'Hardened'
-                    ,4:'Veteran'
-                    ,5:'Legend'}
-        
-        analytics = ['Numpy','Scipy','Pandas','Matplotlib','ScikitLearn','TensorFlow','Tidyverse']
-        devs = ['Flask','SQL Connectors','Docker','Git-GitHub']
-        bi = ['Excel','Excel VBA','R Shiny', 'Streamlit']
-        storage = ['Postgres','SQLServer','SQLite', 'Bash']
         
         with st.container():
             col1, col2 = st.columns((1,1))
 
             with col1:
-                fig = plt.figure()
-                ax = fig.add_subplot()
+                # Load previously generated image
+                fig_ = Image.open(settings.config['images']['analytics'])
 
-                ax.barh(
-                    y=analytics
-                    ,width=[4,4,5,5,4,3,2]
-                    ,color='green'
-                    ,alpha=0.5)
-
-                ax.set_title('Advanced Analytics', fontsize=18)
-                ax.set_xticks(
-                    ticks=list(skill_level.keys())
-                    ,labels=list(skill_level.values()))
-                ax.set_xlim(right=max(skill_level.keys()))
-                ax.spines['top'].set_visible(False)
-                ax.spines['right'].set_visible(False)
-                ax.spines['left'].set_visible(False)
-                
-                #Add plot to streamlit
-                st.pyplot(
-                    fig=fig
-                    ,clear_figure=True)
+                # Render image
+                st.image(fig_)
             
             with col2:
-                fig = plt.figure()
-                ax = fig.add_subplot()
+                # Load previously generated image
+                fig_ = Image.open(settings.config['images']['development'])
 
-                ax.barh(
-                    y=devs
-                    ,width=[1,4,2,5]
-                    ,color='red'
-                    ,alpha=0.5)
-
-                ax.set_title('Development', fontsize=18)
-                ax.set_xticks(
-                    ticks=list(skill_level.keys())
-                    ,labels=list(skill_level.values()))
-                ax.set_xlim(right=max(skill_level.keys()))
-                ax.spines['top'].set_visible(False)
-                ax.spines['right'].set_visible(False)
-                ax.spines['left'].set_visible(False)
-
-                #Add plot to streamlit
-                st.pyplot(
-                    fig=fig
-                    ,clear_figure=True)
-        
+                # Render image
+                st.image(fig_)
 
         with st.container():
             col1, col2 = st.columns((1,1))
 
             with col1:
-                fig = plt.figure()
-                ax = fig.add_subplot()
+                # Load previously generated image
+                fig_ = Image.open(settings.config['images']['bi'])
 
-                ax.barh(
-                    y=bi
-                    ,width=[5,3,2,5]
-                    ,color='blue'
-                    ,alpha=0.5)
-
-                ax.set_title('Bussines Inteligence', fontsize=18)
-                ax.set_xticks(
-                    ticks=list(skill_level.keys())
-                    ,labels=list(skill_level.values()))
-                ax.set_xlim(right=max(skill_level.keys()))
-                ax.spines['top'].set_visible(False)
-                ax.spines['right'].set_visible(False)
-                ax.spines['left'].set_visible(False)
-                
-                #Add plot to streamlit
-                st.pyplot(
-                    fig=fig
-                    ,clear_figure=True)
+                # Render image
+                st.image(fig_)
             
             with col2:
-                fig = plt.figure()
-                ax = fig.add_subplot()
+                # Load previously generated image
+                fig_ = Image.open(settings.config['images']['cloud'])
 
-                ax.barh(
-                    y=storage
-                    ,width=[4,4,3,1]
-                    ,color='purple'
-                    ,alpha=0.5)
-
-                ax.set_title('Data Processing & Storage', fontsize=18)
-                ax.set_xticks(
-                    ticks=list(skill_level.keys())
-                    ,labels=list(skill_level.values()))
-                ax.set_xlim(right=max(skill_level.keys()))
-                ax.spines['top'].set_visible(False)
-                ax.spines['right'].set_visible(False)
-                ax.spines['left'].set_visible(False)
-
-                #Add plot to streamlit
-                st.pyplot(
-                    fig=fig
-                    ,clear_figure=True)
+                # Render image
+                st.image(fig_)
         
         st.write("###")
