58e6f6206a64c5dd8c4488d1f4d66e2201a4a9f5
CAMM961001 <miguel.castama@gmail.com>
2023-05-11 14:03:36 -0500

    Professional summary added to page description

--- src/about_me.py
+++ src/about_me.py
@@ -10,5 +10,14 @@ class AboutMe:
     def __init__(self):
         self.page_title = 'About Me'
         self.descripcion = '''
-        Description
+        <p style="text-align: justify; font-size: {settings.fontsize}px">
+            <br>
+            I am a data scientist experienced in manufacturing processes, and 
+            automation of financial services back-end processes. My specialization 
+            covers end-to-end data analytics solutions, from data engineering to 
+            Business Intelligence. I am interested on professional challenges 
+            involving Machine Learning and Statistical Modeling to leverage 
+            data-driven decisions.
+            </br>
+        </p>
         '''
