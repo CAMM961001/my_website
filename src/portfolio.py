6cceaed6f8509f21ddf967beacf8746a6a7bbbab
CAMM961001 <miguel.castama@gmail.com>
2023-05-11 21:07:54 -0500

    Pep8

--- src/portfolio.py
+++ src/portfolio.py
@@ -20,15 +20,25 @@ class Portfolio:
     def first_row(self):
         with st.container():
             col1, col2, col3= st.columns(3, gap='large')
+
             with col1:
-                cover = Image.open(self.data[0]['cover'])
+                # Project header and cortibutors
                 st.subheader(self.data[0]['project_name'])
                 st.caption(self.data[0]['contributors'])
+                
+                # Project cover image
+                cover = Image.open(self.data[0]['cover'])
                 st.image(cover)
+
+                # Project description content
                 description = ""
                 for line in self.data[0]['description']:
                     description += line
-                description = f'''<p style="text-align: justify; font-size: 16px">{description}</p>'''
-                st.markdown(description, unsafe_allow_html=True)
+                description = f'''<p style="text-align: justify">{description}</p>'''
+
+                # Render description
+                st.write(description, unsafe_allow_html=True)
+
+                # Project navigation buttons
                 render_button(url=self.data[0]['site_url'])
                 render_button(url=self.data[0]['source_url'], name='View source')
